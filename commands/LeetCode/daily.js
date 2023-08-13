6b7c6f8fd9d290fc312d03195e9873a8dac03575
Hunter H <hunter.hester15@gmail.com>
2023-08-13 00:00:45 -0500

    adds functionality to daily command

--- commands/LeetCode/daily.js
+++ commands/LeetCode/daily.js
@@ -1,24 +1,32 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { fetchDailyCodingChallenge } = require('../../util/dailyquery.js'); 
 
-const test = async () => {
+const fetchData = async () => {
     try {
         const jsonDATA = await fetchDailyCodingChallenge();
-        console.log(jsonDATA.data.activeDailyCodingChallengeQuestion.question.title)
         const dailyTitle = jsonDATA.data.activeDailyCodingChallengeQuestion.question.title;
         const dailyURL = `https://leetcode.com${jsonDATA.data.activeDailyCodingChallengeQuestion.link}`;
+        return { dailyTitle, dailyURL };
     } catch (err) {
-        console.error('Error fetching:', err)
+        console.error('Error fetching:', err);
+        return null;
     }
-}
+};
 
-test();
+// fetchData();
 
 module.exports = {
     data: new SlashCommandBuilder()
     .setName('daily')
     .setDescription('Provides daily leetcode challenge URL'),
     async execute(interaction) {
-        await interaction.reply(`Today's daily is ${dailyTitle}, URL: ${dailyURL} `)
+        const { dailyTitle, dailyURL } = await fetchData();
+
+        if (dailyTitle && dailyURL) {
+            await interaction.reply(`Today's daily is ${dailyTitle}, URL: ${dailyURL} `)
+        } else {
+            await interaction.reply("Failed to fetch daily challenge data.");
+        }
+        
     },
 }
